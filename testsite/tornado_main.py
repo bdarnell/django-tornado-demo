0a85fc2ac25cc36334bd1bbc1484a28d0cae3537
Ben Darnell <bdarnell@beaker.local>
2010-02-05 12:38:14 -0800

    Add a tornado_main.py file

---
+++ testsite/tornado_main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Run this with
+# PYTHONPATH=. DJANGO_SETTINGS_MODULE=testsite.settings testsite/tornado_main.py
+# Serves by default at
+# http://localhost:8080/hello-tornado and
+# http://localhost:8080/hello-django
+
+from tornado.options import options, define, parse_command_line
+import django.core.handlers.wsgi
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.wsgi
+
+define('port', type=int, default=8080)
+
+class HelloHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write('Hello from tornado')
+
+def main():
+  wsgi_app = tornado.wsgi.WSGIContainer(
+    django.core.handlers.wsgi.WSGIHandler())
+  tornado_app = tornado.web.Application(
+    [
+      ('/hello-tornado', HelloHandler),
+      ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+      ])
+  server = tornado.httpserver.HTTPServer(tornado_app)
+  server.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+  main()
